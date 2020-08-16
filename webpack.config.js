c98051ec775050ac4475976dc85970c805b03f5e
Karthik Vadlapatla <vadlapatla.karthik@gmail.com>
2020-08-16 06:56:19 +0530

    moved css ooutput to a differnet directory

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         //         template: path.resolve(__dirname, "src", "index.html"),
         //     }),
         new MiniCssExtractPlugin({
-            filename: "../style.css",
+            filename: "../css/style.css",
         }),
     ],
 };
